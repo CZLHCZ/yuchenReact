5ea1821f7dcb994254ec923ed2b8016719a5a0a0
yuchen <373428582@qq.com>
2020-02-25 16:20:11 +0800

    浅复制

--- src/components/LifeCycle.js
+++ src/components/LifeCycle.js
@@ -52,19 +52,33 @@ class Lifecycle extends Component {
 
         console.log('01构造函数');
         super(props);
-        this.state = { 
+        this.state = {
 
-            msg:'我是一个msg'
-         };
-    }  
+            msg: '我是一个msg'
+        };
+    }
 
     //组件将要挂载的时候触发的生命周期函数
-    UNSAFE_componentWillMount(){
-
+    UNSAFE_componentWillMount() {
+        let testa = {
+            age: 18,
+            name: 'aaa',
+            city:{
+                address:"金陵"
+            }
+        }
+        let testb= testa;
+        testb.age=20;
+        console.log(testa);
+        console.log(testb)
+        // let testc={...testa,...testb}
+        //等同于
+        //let cc = Object.assign({}, aa, bb);
+        // console.log(testc) 
         console.log('02组件将要挂载');
     }
     //组件挂载完成的时候触发的生命周期函数
-    componentDidMount(){
+    componentDidMount() {
 
         //dom操作放在这个里面    请求数据也放在这个里面
 
@@ -73,7 +87,7 @@ class Lifecycle extends Component {
 
 
     //是否要更新数据  如果返回true才会执行更新数据的操作
-    shouldComponentUpdate(nextProps, nextState){
+    shouldComponentUpdate(nextProps, nextState) {
         console.log('01是否要更新数据');
 
         console.log(nextProps);
@@ -85,37 +99,37 @@ class Lifecycle extends Component {
 
     //将要更新数据的时候触发
 
-    UNSAFE_componentWillUpdate(){
+    UNSAFE_componentWillUpdate() {
         console.log('02组件将要更新');
     }
     //组件更新完成
-    componentDidUpdate(){
+    componentDidUpdate() {
         console.log('04组件数据更新完成');
     }
 
     // 你在父组件里面改变props传值的时候触发的
 
-    UNSAFE_componentWillReceiveProps(){
+    UNSAFE_componentWillReceiveProps() {
 
         console.log('父子组件传值，父组件里面改变了props的值触发的方法')
     }
 
-    setMsg=()=>{
+    setMsg = () => {
 
         this.setState({
 
-            msg:'我是改变后的msg的数据'
+            msg: '我是改变后的msg的数据'
         })
     }
 
     //组件销毁的时候触发的生命周期函数   用在组件销毁的时候执行操作
-    componentWillUnmount(){
+    componentWillUnmount() {
 
-            console.log('组件销毁了');
+        console.log('组件销毁了');
     }
     render() {
         console.log('03数据渲染render');
-       
+
         return (
             <div>
 
